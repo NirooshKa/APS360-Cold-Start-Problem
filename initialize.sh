d104ec699846d2632a4aff69a7acfa8daa45f777
Charles Zhang <szinubuntu@gmail.com>
2021-03-07 14:08:52 -0500

    Add bash shell script for initializing YOLO environment

---
+++ initialize.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Clone darknet into current folder and build the project
+git clone https://github.com/AlexeyAB/darknet.git
+cd darknet
+make
+# Download configuration file
+wget https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov4.cfg
+# Download weight file
+wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights
+
+# Run below for testing purpose
+# ./darknet detector test cfg/coco.data yolov4.cfg yolov4.weights -ext_output data/dog.jpg
