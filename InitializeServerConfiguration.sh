0cc6a5f7470a8c32e992b03c939c4e3cf04bae66
Charles Zhang <Charles@totalimagine.com>
2021-03-31 05:57:42 +0000

    Add initialization for Pytorch on server

--- InitializeServerConfiguration.sh
+++ InitializeServerConfiguration.sh
@@ -1,3 +1,4 @@
 printf "PythonPath: $(which python3)\n" > ServerConfigurations.yaml
 printf "PythonCommands: [\"$(pwd)/yolostage.py\", \"$(pwd)/work/work.png\"]" >> ServerConfigurations.yaml
 apt-get update && apt-get install -y libgdiplus
+pip3 install torch==1.8.1+cpu torchvision==0.9.1+cpu torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
