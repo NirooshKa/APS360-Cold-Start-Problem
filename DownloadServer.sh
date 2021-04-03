a2082657b0838648aee3b75c153fb886dbee50cb
Charles Zhang <szinubuntu@gmail.com>
2021-04-02 22:58:02 -0400

    Update shell script

--- DownloadServer.sh
+++ DownloadServer.sh
@@ -2,8 +2,7 @@
 rm -fr Server
 rm Server.zip
 # Download new archive
-# wget -O Server.zip https://github.com/NirooshKa/APS360-Cold-Start-Problem/releases/download/V0.2/Server.zip # Release with only YOLOStage
-wget -O Server.zip https://github.com/NirooshKa/APS360-Cold-Start-Problem/releases/download/V0.3/Server.zip # Latest
+wget -O Server.zip https://github.com/NirooshKa/APS360-Cold-Start-Problem/releases/download/V0.2/Server.zip
 unzip Server.zip -d Server
 # Clean archive
 rm Server.zip
